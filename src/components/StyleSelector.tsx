bd696348a43c2b81b4fa5ee9eefaa27d838c673a
Sathvika143 <saiamroji23@gmail.com>
2026-01-14 12:47:12 +0530

    3

---
+++ src/components/StyleSelector.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import type { ThumbnailStyle } from "../assets/assets";
+import {
+  ChevronDownIcon,
+  CpuIcon,
+  ImageIcon,
+  PenToolIcon,
+  SparkleIcon,
+  SquareIcon,
+} from "lucide-react";
+
+const StyleSelector = ({
+  value,
+  onChange,
+  isOpen,
+  setIsOpen,
+}: {
+  value: ThumbnailStyle;
+  onChange: (style: ThumbnailStyle) => void;
+  isOpen: boolean;
+  setIsOpen: () => void;
+}) => {
+  const styleDescriptions: Record<ThumbnailStyle, string> = {
+    "Bold & Graphic": "High contrast, bold typography, striking visuals",
+    Minimalist: "High contrast, bold typography , striking visuals",
+    Photorealistic: "Photo-based, natural looking",
+    Illustrated: "Hand-drawn, artistic, creative",
+    "Tech/Futuristic": "Modern, sleek, tech-inspired",
+  };
+  const styleIcons: Record<ThumbnailStyle, React.ReactNode> = {
+    "Bold & Graphic": <SparkleIcon className="h-4 w-4" />,
+    Minimalist: <SquareIcon className="h-4 w-4" />,
+    Photorealistic: <ImageIcon className="h-4 w-4" />,
+    Illustrated: <PenToolIcon className="h-4 w-4" />,
+    "Tech/Futuristic": <CpuIcon className="h-4 w-4" />,
+  };
+
+  return (
+    <div className="relative space-y-3 dark">
+      <label className="block text-sm font-medium">Thumbnail Style</label>
+      <button
+        type="buton"
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex w-full items-center justify-between rounded-md border px-4
+        py-3 text-left transition bg-white/8 border-white/10 text-zinc-200 hover:bg-white/12"
+      >
+        <div className="space-y-1">
+          <div className="flex items-center gap-2 font-medium">
+            {styleIcons[value]}
+            <span>{value}</span>
+          </div>
+          <p className="text-xs text-zinc-400">{styleDescriptions[value]}</p>
+        </div>
+        <ChevronDownIcon
+          className={[
+            "h-5 w-5 text-zinc-400 transition-transform",
+            isOpen && "rotate-180",
+          ].join(" ")}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default StyleSelector;
